9c7a59efdf3e64755f2e9bf82d1cb2a3680660ee
deshpatharva <92350098+deshpatharva@users.noreply.github.com>
2026-06-07 10:38:17 -0600

    Update variables.tf

--- resume-optimizer/infra/variables.tf
+++ resume-optimizer/infra/variables.tf
@@ -43,7 +43,7 @@ variable "postgres_db_name" {
 variable "postgres_sku" {
   description = "PostgreSQL Flexible Server SKU (student account: Standard_B1ms ~$12/mo)"
   type        = string
-  default     = "Standard_B1ms"
+  default     = "B_Standard_B1ms"
 }
 
 variable "postgres_storage_mb" {
