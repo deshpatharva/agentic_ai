b2cdf904e4340bea2d61dcf502f30e78f4f05a00
deshpatharva <92350098+deshpatharva@users.noreply.github.com>
2026-06-07 20:31:45 +0000

    style: auto-format terraform files

--- resume-optimizer/infra/resource_group.tf
+++ resume-optimizer/infra/resource_group.tf
@@ -1,6 +1,6 @@
 import {
-    to = azurerm_resource_group.main
-    id = "/subscriptions/6beb02cf-15a2-4da3-bf0d-e18eeb75d08b/resourceGroups/resumeai-rg-dev"
+  to = azurerm_resource_group.main
+  id = "/subscriptions/6beb02cf-15a2-4da3-bf0d-e18eeb75d08b/resourceGroups/resumeai-rg-dev"
 }
 resource "azurerm_resource_group" "main" {
   name     = local.resource_group_name
